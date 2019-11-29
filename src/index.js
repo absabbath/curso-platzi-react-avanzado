681bf105aaf0601c4e7f0c4c0f9b87212c9590a1
Angel Martin <angelmartin.isc@gmail.com>
2019-11-29 16:44:23 -0600

    apollo react list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-tuxmart-2qz5rz6tn.now.sh/graphql'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
